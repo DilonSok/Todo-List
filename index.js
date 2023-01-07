e11d7c4dd5510d37ff8b13403af79457b843b865
Dilon Sok <sokdilon@gmail.com>
2023-01-07 04:14:29 -0600

    update 1.1
    Differentiated between previous version, however might look towards different way of adding and displaying elements of todo items.
    
    Current way creates complicated code  to style todo elements versus just a simple "todo.title + ' ' + todo.dueDate"
    
    Going to start new branch called , temp 1.0 with this in mind and go for different approach instead.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let todos = []; //Todo-List array of todo-elements
 
-      //const uid = function(){
-      //  return Date.now().toString(36) + Math.random().toString(36).substr(2);
-      //}
+      // const uid = function(){
+      //   return Date.now().toString(36) + Math.random().toString(36).substr(2);
+      // }
       
       function addTodo(){
         const textBox = document.getElementById("todo-title");
@@ -16,50 +16,56 @@ let todos = []; //Todo-List array of todo-elements
       
       }
       function createTodo(title, dueDate){
+        const id = '' + new Date().getTime();;
         todos.push({
           title: title,
           dueDate: dueDate,
+          id: id
         });
       }
-      
-      /**
-       * I want this to be able to recreate the input div underneath an element when you add something.
-       * Issue is that when adding a button property to the the addButton, it creates an infinite loop.
-       * I am going to create a branch that instead just works as most typical todo-lists that has a 
-       * static add task bar instead of this version, which aims to add the bar beneath etc.
-       */
-      function recreateInput(){
-        
-        const titleInput = document.createElement('input');
-        titleInput.type = "text";
-        const dateInput = document.createElement('input')
-        dateInput.type = "date";
-        
-        const addButton = document.createElement('button');
-        const addText = document.createTextNode('Add');
-        addButton.appendChild(addText);
-        addButton.addEventListener('click', addTodo);
 
-        const add = document.createElement('div');
-        add.appendChild(titleInput);
-        add.appendChild(dateInput);
-        add.appendChild(addButton);
-        
-        const todoList = document.getElementById('element-container');
-        todoList.appendChild(add);
+      function removeTodo(idToDelete){
+        todos = todos.filter(function (todo){
+          if(todo.id === idToDelete){
+            return false;
+          }
+          else{
+            return true;
+          }
+        });
+      }
+      function deleteTodo(event){
+        const deleteButton = event.target;
+        const idToDelete = deleteButton.id;
+
+        removeTodo(idToDelete);
+
+        render();
+      }
+      
+      function createDeleteButton(){
+          const deleteButton = document.createElement('button');
+          deleteButton.innerText = 'X';
+          deleteButton.style = 'margin-left: 12px';
+          deleteButton.onclick = deleteTodo;
+          return deleteButton;
       }
       
       
       function render(){
-        document.getElementById("element-container").innerHTML = null;
+        document.getElementById("element-container").innerHTML = '';
       
         todos.forEach(todo => {
           const currentElement = document.createElement('div');
           currentElement.innerText = todo.title + ' ' + todo.dueDate;
+
+          const deleteButton = createDeleteButton();
+          deleteButton.id = todo.id;
+          currentElement.appendChild(deleteButton);
+          
       
           const todoList = document.getElementById('element-container');
           todoList.appendChild(currentElement);
       
         });
-        recreateInput();
       }
